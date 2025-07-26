87992cf69ae66c1d08eb59b6de83a814b92c6e3b
Siddhartha <srisaisiddharthamanepalli@gmail.com>
2025-07-26 15:48:53 +0530

    upgrading project to mini cart

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect, url_for, request, render_template
+from flask import Flask, session, redirect, url_for, request, render_template # type: ignore
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
